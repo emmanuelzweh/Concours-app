ce7e19a92a274ae06cceb75b0fb809071b5ca64c
emmanuelzweh <emmanuelzweh80@gmail.com>
2023-08-05 23:53:38 -0400

    College list version 1 and Splash screen setup done

--- screens/CollegeListScreen.js
+++ screens/CollegeListScreen.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// CollegeListScreen.js
+import React from 'react';
+import { View, Text, TouchableOpacity, ImageBackground, StyleSheet, ScrollView } from 'react-native';
 
-function CollegeListScreen() {
+const colleges = [
+  { id: 1, name: 'College A', logo: require('../assets/colleges/EDP.png') },
+  { id: 2, name: 'College B', logo: require('../assets/colleges/EDP.png') },
+  { id: 3, name: 'College C', logo: require('../assets/colleges/EDP.png') },
+  { id: 4, name: 'College D', logo: require('../assets/colleges/EDP.png') },
+  { id: 5, name: 'College E', logo: require('../assets/colleges/EDP.png') },
+  { id: 6, name: 'College F', logo: require('../assets/colleges/EDP.png') },
+  { id: 7, name: 'College G', logo: require('../assets/colleges/EDP.png') },
+  // Add more colleges
+];
+
+const CollegeListScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>College list</Text>
-    </View>
-  )
-}
+    <ScrollView style={styles.container}>
+      {colleges.map((college) => (
+        <TouchableOpacity key={college.id} onPress={() => navigation.navigate('CollegeDetails', { college })}>
+          <ImageBackground source={college.logo} style={styles.collegeItem}>
+            <Text style={styles.collegeName}>{college.name}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  );
+};
 
-export default CollegeListScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0', // Set the background color of the college list screen
+  },
+  collegeItem: {
+    width: '100%',
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.7)', // Dark overlay to make text readable
+  },
+  collegeName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff', // Text color on the dark overlay
+    padding: 10,
+  },
+});
+
+export default CollegeListScreen;
