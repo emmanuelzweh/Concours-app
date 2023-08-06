2dc618cf60998dc442e31c8dc057877323d9255b
emmanuelzweh <emmanuelzweh80@gmail.com>
2023-08-05 23:03:20 -0400

    Colleges stack navigation version1

---
+++ screens/CollegeListScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function CollegeListScreen() {
+  return (
+    <View>
+      <Text>College list</Text>
+    </View>
+  )
+}
+
+export default CollegeListScreen
--- screens/CollegesScreen.js
+++ screens/CollegesScreen.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
 import { View, Text } from 'react-native'
+
+// importing the screens for this stack navigation
+import CollegeDetailsScreen from './CollegeDetailsScreen';
+import CollegeListScreen from './CollegeListScreen';
+
+const CollegesStack = createStackNavigator();
 
 function CollegesScreen() {
   return (
-    <View>
-      <Text>Colleges</Text>
-    </View>
+    <CollegesStack.Navigator initialRouteName="CollegeList">
+        <CollegesStack.Screen name="CollegeList" component={CollegeListScreen} />
+        <CollegesStack.Screen name="CollegeDetails" component={CollegeDetailsScreen} />
+    </CollegesStack.Navigator>
   )
 }
 
