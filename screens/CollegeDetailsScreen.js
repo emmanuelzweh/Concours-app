a15ff48bb829dd3586c682ec6741d3e825270aee
emmanuelzweh <emmanuelzweh80@gmail.com>
2023-08-12 18:48:37 -0400

    Programs details screen is done

--- screens/CollegeDetailsScreen.js
+++ screens/CollegeDetailsScreen.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// import React from 'react'
+// import { View, Text } from 'react-native'
 
-function CollegeDetailsScreen() {
+// function CollegeDetailsScreen() {
+//   return (
+//     <View>
+//       <Text>Colleges Details CollegesScreen</Text>
+//     </View>
+//   )
+// }
+
+// export default CollegeDetailsScreen
+
+// CollegeDetailsScreen.js
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+const CollegeDetailsScreen = ({ route }) => {
+  const { college } = route.params;
+
   return (
     <View>
-      <Text>Colleges Details CollegesScreen</Text>
+      <Image source={college.logo} />
+      <Text>{college.name}</Text>
+      {/* Display more details about the selected college */}
     </View>
-  )
-}
+  );
+};
 
-export default CollegeDetailsScreen
+export default CollegeDetailsScreen;
--- screens/ProgramDetailsScreen.js
+++ screens/ProgramDetailsScreen.js
@@ -1,12 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native'
 
-function ProgramDetailsScreen() {
+import React from 'react';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+
+const ProgramDetailsScreen = ({ route }) => {
+  const { program } = route.params;
+
   return (
-    <View>
-      <Text>Colleges Details CollegesScreen</Text>
-    </View>
-  )
-}
+    <ScrollView>
+      <Image style={styles.programDetailHeaderImage} source={program.logo} />
+      <Text style={styles.title}>{program.name}</Text>
+      <Text style={styles.desc}>
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+      printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+       only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more 
+      recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </Text>
+      <Text style={styles.desc}>
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown 
+      printer took a galley of type and scrambled it to make a type specimen book. It has survived not
+       only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more 
+      recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </Text>
+    </ScrollView>
+  );
+};
 
-export default ProgramDetailsScreen
+const styles = StyleSheet.create({
+  programDetailHeaderImage: {
+    width: '100%'
+  },
+  title: {
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    fontSize: 25,
+    fontWeight: 'bold',
+    
+  },
+  desc: {
+    paddingHorizontal: 16,
+    justifyContent: 'center',
+    fontSize: 18,
+    paddingBottom: 20,
+    textAlign: 'justify'
+  
+
+  }
+})
+
+export default ProgramDetailsScreen;
